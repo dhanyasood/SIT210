ef7847db4970dcd6eb26b73b4a00c9fa3c0b3081
dhanyasood <dhanya28sood@gmail.com>
2026-08-20 22:20:00 +0530

    Create secrets.h for private credentials
    Added a new header file to store private credentials.

---
+++ Task2.1WebHook/secrets..h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Parul"		
+#define SECRET_PASS "******"	
+
+#define SECRET_CH_ID  3462853			
+#define SECRET_WRITE_APIKEY "16POY8XO5J1IOX5K"   
